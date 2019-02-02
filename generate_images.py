ab98fe38bd7ef487c529334fe56c7953ae01b179
Mirgahney <mmerghaney@aimsammi.org>
2019-02-02 08:45:17 +0200

    Content and Style Features

--- generate_images.py
+++ generate_images.py
@@ -73,3 +73,29 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 # content and style ims side-by-side
 ax1.imshow(im_convert(content))
 ax2.imshow(im_convert(style))
+
+### Content and Style Features ###
+def get_features(image, model, layers=None):
+    """ Run an image forward through a model and get the features for 
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    
+    ## Need the layers for the content and style representations of an image
+    if layers is None:
+        layers = {'0': 'conv1_1',
+                  '5': 'conv2_1',
+                  '10': 'conv3_1',
+                  '19': 'conv4_1',
+                  '21': 'conv4_2',
+                  '28': 'conv5_1',
+                 }
+
+    features = {}
+    x = image
+    # model._modules is a dictionary holding each module in the model
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+            
+    return features
