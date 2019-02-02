fee5d02496def9f05490995cf35b410935ef9c8a
Mirgahney <mmerghaney@aimsammi.org>
2019-02-02 08:43:16 +0200

    Adding heper functions

---
+++ generate_images.py
@@ -0,0 +1,75 @@
+# import resources
+%matplotlib inline
+
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.optim as optim
+from torchvision import transforms, models
+
+
+## Loading Model (need to be changed)
+# get the "features" portion of VGG19 (we will not need the "classifier" portion)
+vgg = models.vgg19(pretrained=True).features
+
+# freeze all VGG parameters since we're only optimizing the target image
+for param in vgg.parameters():
+    param.requires_grad_(False)
+
+ # move the model to GPU, if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vgg.to(device)
+
+# Helper functions
+def load_image(img_path, max_size=400, shape=None):
+    ''' Load in and transform an image, making sure the image
+       is <= 400 pixels in the x-y dims.'''
+    
+    image = Image.open(img_path).convert('RGB')
+    
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+    
+    if shape is not None:
+        size = shape
+        
+    in_transform = transforms.Compose([
+                        transforms.Resize(size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), 
+                                             (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+    
+    return image
+
+# helper function for un-normalizing an image 
+# and converting it from a Tensor image to a NumPy image for display
+def im_convert(tensor):
+    """ Display a tensor as an image. """
+    
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1)
+
+    return image
+
+# load in content and style image
+content = load_image('Images/octopus.jpg').to(device)
+# Resize style to match content, makes code easier
+style = load_image('Images/hockney.jpg', shape=content.shape[-2:]).to(device)
+
+# display the images
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+# content and style ims side-by-side
+ax1.imshow(im_convert(content))
+ax2.imshow(im_convert(style))
